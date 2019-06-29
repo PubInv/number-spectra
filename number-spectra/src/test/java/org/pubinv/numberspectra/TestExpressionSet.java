c49ec7087c69d438b9280a354ef3d5c8da6cb94f
egoff <egoff@affinipay.com>
2019-06-29 15:30:38 -0500

    add E2

--- number-spectra/src/test/java/org/pubinv/numberspectra/TestExpressionSet.java
+++ number-spectra/src/test/java/org/pubinv/numberspectra/TestExpressionSet.java
@@ -2,7 +2,9 @@ package org.pubinv.numberspectra;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 
 import org.junit.Test;
 
@@ -10,6 +12,18 @@ public class TestExpressionSet {
 
 	@Test
 	public void testE0() {
-		assertEquals(ExpressionSet.E0.expressions, Collections.emptySet());
+		assertEquals(ExpressionSet.generateE0().expressions, Collections.emptySet());
+	}
+
+	@Test
+	public void testE1() {
+		assertEquals(ExpressionSet.generateE1().expressions, new HashSet<Expr>(Arrays.asList(new One())));
+	}
+
+	@Test
+	public void testE2() {
+		assertEquals(ExpressionSet.generateE2().expressions, new HashSet<Expr>(Arrays.asList(
+				new Factorial(new One()),
+				new Negate(new One()))));
 	}
 }
