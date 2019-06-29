37cad289e546bb427124e8d4e4a7d91aa1051b96
egoff <egoff@affinipay.com>
2019-06-29 14:30:55 -0500

    first expression swag

---
+++ number-spectra/src/main/java/org/pubinv/Expr.java
@@ -0,0 +1,9 @@
+package org.pubinv;
+
+/**
+ * Base for all expressions.
+ */
+public interface Expr {
+	public int hashCode();
+	public boolean equals(Object o);
+}
---
+++ number-spectra/src/main/java/org/pubinv/One.java
@@ -0,0 +1,20 @@
+package org.pubinv;
+
+public class One implements Expr {
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		return true;
+	}
+
+}
